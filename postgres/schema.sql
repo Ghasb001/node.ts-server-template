5e93b4842e0cfad07601d7e8d4c5e1b3ea31ea33
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-12-08 10:51:16 -0500

    clean

--- postgres/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE spectrum;
-
-CREATE TABLE leaders (
-      leader_id serial PRIMARY KEY,
-      Name text NOT NULL,
-      title text NOT NULL,
-      postNomials text[],
-      picture text not null
-    );
