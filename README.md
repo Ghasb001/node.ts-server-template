302ec1b1c778cf927e2aec9882c101013bbbca4d
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-12-12 09:14:01 -0500

    work in index.ts for both connections

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ This is a quick Node.js server template, written out using Typescript.
 
 1. Clone repository
 2. Install dependencies
+3. Use the appropriate connections for your selected DBMS
--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import axios from 'axios';
+// DBMS connection imports
+import Pool from './postgres/db';
 import { connectToDatabase, collections } from './mongo/mongo';
-import Leaders from "./mongo/models/leaders";
-import TipsAndTricks from "./mongo/models/tipsTricks";
-import Resources from "./mongo/models/resources";
+
 dotenv.config();
 
 const app: Express = express();
@@ -16,95 +16,26 @@ app.get('/', (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
+// mongoDB connection function
 connectToDatabase();
 
-app.get('/leaders', async (req: Request, res: Response) => {
-  try {
-    const leads = (await collections.leaders.find({}).toArray()) as Leaders[];
-    leads.sort((a, b) => a.priority - b.priority)
-    res.status(200).send(leads);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.get('/tipstricks', async (req: Request, res: Response) => {
-  try {
-    const tip = (await collections.tipstricks.find({}).toArray()) as TipsAndTricks[];
-    res.status(200).send(tip);
-  } catch (error) {
-    console.log(error)
-    res.status(500).send(error.message);
-  }
-});
-
-app.get('/resources', async (req: Request, res: Response) => {
-  var sorted = {
-    providers: [],
-    community: [],
-    services: [],
-  };
-  try {
-    const resc = (await collections.resources.find({}).toArray()) as Resources[];
-    resc.forEach((x) => {
-      if (x.type === 'ABA Provider') { sorted.providers.push(x) }
-      else if (x.type === 'Community Resource') { sorted.community.push(x) }
-      else { sorted.services.push(x) }
-    })
-    res.status(200).send(sorted);
-  } catch (error) {
-    console.log(error)
-    res.status(500).send(error.message);
-  }
-});
-
-app.post('/usage', async (req: Request, res: Response) => {
-  try {
-    console.log(req)
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.get('/kideos', (req: Request, res: Response) => {
-  axios.get('https://www.googleapis.com/youtube/v3/search', {
-    params: {
-      q: 'ms. rachel',
-      part: 'snippet',
-      maxResults: '17',
-      key: `${process.env.YT_KEY}`
-    }
-  })
-    .then((vids) => {
-      res.status(200).send(vids.data.items);
-    })
-    .catch((err) => {
-      console.log(err.headers)
-      res.sendStatus(500);
-    })
+app.get('/pgExample', async (req: Request, res: Response) => {
+  Pool.query("SELECT * FROM example_table")
+  .then((data: any) => res.send(data.rows).status(201))
+  .catch((err: any) => res.sendStatus(500))
 })
 
-app.get('/careVids', (req: Request, res: Response) => {
-  axios.get('https://www.googleapis.com/youtube/v3/search', {
-    params: {
-      q: 'ABA for parents',
-      part: 'snippet',
-      maxResults: '17',
-      key: `${process.env.YT_KEY}`
-    }
-  })
-    .then((vids) => {
-      res.status(200).send(vids.data.items);
-    })
-    .catch((err) => {
-      console.log(err.headers)
-      res.sendStatus(500);
-    })
-})
+// app.get('/mongoExample', async (req: Request, res: Response) => {
+//   try {
+//     const leads = (await collections.leaders.find({}).toArray()) as Leaders[];
+//     leads.sort((a, b) => a.priority - b.priority)
+//     res.status(200).send(leads);
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// });
 
-app.get(`/${process.env.loader}`, async (req: Request, res: Response) => {
-  res.send(`${process.env.loader}`);
-});
+
 
 const port = process.env.PORT || 1128;
 app.listen(port, () => {
